474428c713d96e5d76970e67e619adcf05c67960
Timothy Wewengkang <timothy.yeremia.wewengkang@gmail.com>
2024-08-13 23:37:50 +0000

    fix page error for hypothesis page

--- app_pages/summary.py
+++ app_pages/summary.py
@@ -11,7 +11,7 @@ def summary_body():
         )
 
     st.success(
-        'The project has 2 business requirements:\n'
-        '*1. The client is interested in discovering how the house attributes correlate with the sale price.\n'
-        '*2. The client is interested in predicting the house sale price from her four inherited houses and any other house in Ames, Iowa.'
+        'The project has 2 business requirements:'
+        '*1. - The client is interested in discovering how the house attributes correlate with the sale price.'
+        '*2. - The client is interested in predicting the house sale price from her four inherited houses and any other house in Ames, Iowa.'
         )
