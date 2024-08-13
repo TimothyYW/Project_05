84aecc03f30522a48b5d7171a90e7509dd6f72a8
Timothy Wewengkang <timothy.yeremia.wewengkang@gmail.com>
2024-08-13 22:16:20 +0000

    add load page script

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 from app_pages.multipage import MultiPage
 
 # load pages scripts
+from app_pages.summary import summary_body
 
-
-app = MultiPage(app_name="House predition")  # Create an instance of the app
+app = MultiPage(app_name="House prediction")  # Create an instance of the app
 
 # Add your app pages here using .add_page()
 
