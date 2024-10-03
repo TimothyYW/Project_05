829622eeade99aa046833ea7a16cc763a7b57ab3
Timothy Wewengkang <timothy.yeremia.wewengkang@gmail.com>
2024-10-03 15:01:40 +0000

    update data_visualization.py

--- app_pages/data_visualization.py
+++ app_pages/data_visualization.py
@@ -1,10 +1,29 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
+import pandas as pd
+# from matplotlib.image import imread
 
-def data_visualization_body():
 
-    st.write('### Data Visualization')
 
-    st.write()
+def sale_price_correlation():
+    
+
+    st.write("Corralations with sale price")
+
+    correlation_analysis = plt.imread(
+        "outputs/correlation_analysis.png")
+    st.image(correlation_analysis,
+            caption='Correlation with sale price')
+    st.write("---")
+
+    st.write("### Model History")
+    
+    model_residual = plt.imread(f"outputs/residual_plot.png")
+    st.image(model_residual, caption='Error in prediction in sale price')
+    
+    st.write("---")
+
+    st.write("### Time of prediction for large and small data set")
+    st.dataframe(pd.DataFrame("outputs/time_prediction.csv"))
+                 
+    st.write("---")
